a623fffb3ce0a3e061ed548a8cfc36788b2a2512
Peter Ruecktenwald <Ruecktenwald@gmail.com>
2024-10-04 16:45:51 -0400

    Remove ignored files from tracking

--- frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Use the base URL from the environment variable
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-
-export const fetchTableData = (endpoint, params = {}) => {
-    return axios.get(`${apiBaseUrl}${endpoint}`, { params });
-};
